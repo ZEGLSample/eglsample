806bd073dd7f56ff941effd1f8cbff747303e708
zhaoshao <tianzhao1989@gmail.com>
2020-02-13 17:07:14 +0800

    Update init_kms.c

--- EGLKMS/01_InitKMS/init_kms.c
+++ EGLKMS/01_InitKMS/init_kms.c
@@ -52,3 +52,22 @@ BOOL init_kms(int fd, struct kms* kms)
 
    return TRUE;
 }
+
+int main(int argc, const char* argv[])
+{
+	int fd;
+	struct kms kms = {0};
+
+	fd = open("/dev/dri/renderD128", O_RDWR);
+	if(fd < 0)
+	{
+		printf("Can't open fd\n");
+		return -1;
+	}
+
+	init_kms(fd, &kms);
+
+	close(fd);
+
+	return 0;
+}
