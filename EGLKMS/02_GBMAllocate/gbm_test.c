7f31261cd7991a17a3bdb601ee5cfb500e3e1e52
zhaoshao <tianzhao1989@gmail.com>
2020-02-13 17:44:14 +0800

    Update gbm_test.c

--- EGLKMS/02_GBMAllocate/gbm_test.c
+++ EGLKMS/02_GBMAllocate/gbm_test.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <gbm.h>
 
+int main(int argc, const char* argv[]){
+	int fd;
+	struct gbm_device *gdevice = NULL;
+	struct gbm_surface *gsurface = NULL;
+	struct gbm_bo *gbo = NULL;
+
+	fd = open("/dev/dri/renderD128", O_RDWR);
+	if(fd < 0){
+		printf("Can't open fd!\n");
+		return -1;
+	}
+
+	gdevice = gbm_create_device(fd);
+	if(!gdevice)
+	{
+		printf("Create gbm device failed!\n");
+		return -1;
+	}
+
+	gsurface = gbm_surface_create(gdevice, 300, 300, GBM_BO_FORMAT_ARGB8888, GBM_BO_USE_RENDRING);
+
+	if(gsurface)
+		gbm_surface_destroy(gsurface);
+
+	if(gdevice)
+		gbm_device_destroy(gdevice);
+
+	close(fd);
+
+	return 0;
+}
