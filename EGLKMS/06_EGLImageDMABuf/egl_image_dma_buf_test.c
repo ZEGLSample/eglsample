da99c395ada49fc312a233611d56dc0e8f71e4b3
zhaoshao <tianzhao1989@gmail.com>
2020-02-26 16:29:14 +0800

    Debug egl image dma buf test case on Ubuntu19.10 to fix some issues

--- EGLKMS/06_EGLImageDMABuf/egl_image_dma_buf_test.c
+++ EGLKMS/06_EGLImageDMABuf/egl_image_dma_buf_test.c
@@ -19,6 +19,11 @@ static EGLSurface eglSurface = NULL;
 static unsigned int prevFb = 0;
 static struct gbm_bo *gBoTest = NULL;
 
+typedef EGLImageKHR (*CreateImageKHR)(EGLDisplay, EGLContext, EGLenum, EGLClientBuffer, const EGLint *);
+typedef EGLBoolean (*DestroyImageKHR)(EGLDisplay, EGLImageKHR);
+CreateImageKHR pfnCreateImageKHR = NULL;
+DestroyImageKHR pfnDestroyImageKHR = NULL;
+
 static const EGLint attribs[] = {
 	EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
 	EGL_RED_SIZE, 8,
@@ -44,6 +49,13 @@ int setup_gbm_egl(void){
 		return -1;
 	}
 
+	pfnCreateImageKHR = (void*)eglGetProcAddress("eglCreateImageKHR");
+	pfnDestroyImageKHR = (void*)eglGetProcAddress("eglDestroyImageKHR");
+	if(!pfnCreateImageKHR || !pfnDestroyImageKHR){
+		printf("eglGetProcAddress CreateImageKHR or DestroyImageKHR failed\n");
+		return -1;
+	}
+
 	eglInitialize(eglDisplay, NULL, NULL);
 
 	eglBindAPI(EGL_OPENGL_API);
@@ -56,7 +68,7 @@ int setup_gbm_egl(void){
 		return -1;
 	}
 
-	gSurface = gbm_surface_create(gDevice, 300, 300, GBM_BO_FORMAT_XRGB8888, GBM_BO_USE_SCANOUT|GBM_BO_USE_RENDERING);
+	gSurface = gbm_surface_create(gDevice, kms.mode.hdisplay, kms.mode.vdisplay, GBM_BO_FORMAT_XRGB8888, GBM_BO_USE_SCANOUT|GBM_BO_USE_RENDERING);
 	if(!gSurface){
 		printf("GBM create surface failed\n");
 		return -1;
@@ -89,7 +101,7 @@ void finish_gbm_egl(void){
 		gbm_device_destroy(gDevice);
 }
 
-void render_egl_image_dma_buf(void){
+void render_egl_image_dma_buf(int width, int height){
 	unsigned int texture;
 	int stride=0, primeFd=0, drmFmt=0;
 	int *pData=NULL, *pRawData=NULL;
@@ -112,7 +124,7 @@ void render_egl_image_dma_buf(void){
 		0.0f, 1.0f  //top left
 	};
 
-	gBoTest = gbm_bo_create(gDevice, 300, 300, GBM_BO_FORMAT_XRGB8888, GBM_BO_USE_RENDERING);
+	gBoTest = gbm_bo_create(gDevice, width, height, GBM_BO_FORMAT_XRGB8888, GBM_BO_USE_RENDERING);
 	if(!gBoTest){
 		printf("Create gbo for test failed\n");
 		return;
@@ -122,11 +134,11 @@ void render_egl_image_dma_buf(void){
 	primeFd = gbm_bo_get_fd(gBoTest);
 	drmFmt = DRM_FORMAT_XRGB8888;
 
-	pRawData = gbm_bo_map(gBoTest, 0, 0, 300, 300, GBM_BO_TRANSFER_READ_WRITE, &mapStride, &mapData);
+	pRawData = gbm_bo_map(gBoTest, 0, 0, width, height, GBM_BO_TRANSFER_READ_WRITE, &mapStride, &mapData);
 	if(pRawData){
 		pData = pRawData;
-		for(i=0; i<300; i++){
-			for(j=0; j<300; j++){
+		for(i=0; i<width; i++){
+			for(j=0; j<height; j++){
 				*pData++ = 0x88888888;
 			}
 			pData = (int*)((char*)pRawData + mapStride*(i+1));
@@ -135,28 +147,28 @@ void render_egl_image_dma_buf(void){
 		gbm_bo_unmap(gBoTest, mapData);
 	}
 
-	glViewport(0, 0, 300, 300);
+	glViewport(0, 0, (GLint)width, (GLint)height);
 
 	glGenTextures(1, &texture);
 	glBindTexture(GL_TEXTURE_2D, texture);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParamateri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParamateri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	glClearColor(0.2f, 0.3f, 0.4f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	int eglAttribs[] = {
 		EGL_LINUX_DRM_FOURCC_EXT, drmFmt,
-		EGL_WIDTH, 300,
-		EGL_HEIGHT, 300,
+		EGL_WIDTH, width,
+		EGL_HEIGHT, height,
 		EGL_DMA_BUF_PLANE0_FD_EXT, primeFd,
 		EGL_DMA_BUF_PLANE0_OFFSET_EXT, 0,
 		EGL_DMA_BUF_PLANE0_PITCH_EXT, stride,
 		EGL_NONE
 	};
-	eglImage = eglCreateImageKHR(eglDisplay, EGL_NO_CONTEXT, EGL_LINUX_DMA_BUF_EXT, NULL, eglAttribs);
+	eglImage = (*pfnCreateImageKHR)(eglDisplay, EGL_NO_CONTEXT, EGL_LINUX_DMA_BUF_EXT, NULL, eglAttribs);
 	if(!eglImage){
 		printf("Create egl image failed\n");
 		if(gBoTest){
@@ -169,7 +181,7 @@ void render_egl_image_dma_buf(void){
 	glBindTexture(GL_TEXTURE_2D, texture);
 	glEGLImageTargetTexture2DOES(GL_TEXTURE_2D, eglImage);
 
-	eglDestroyImageKHR(eglDisplay, eglImage);
+	(*pfnDestroyImageKHR)(eglDisplay, eglImage);
 	gbm_bo_destroy(gBoTest);
 
 	glEnableClientState(GL_VERTEX_ARRAY);
@@ -178,7 +190,7 @@ void render_egl_image_dma_buf(void){
 	glVertexPointer(3, GL_FLOAT, 0, vertices);
 	glTexCoordPointer(2, GL_FLOAT, 0, texCoords);
 
-	glDrawArrays(GL_QUAD, 0, 4);
+	glDrawArrays(GL_QUADS, 0, 4);
 
 	glDisableClientState(GL_VERTEX_ARRAY);
 	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
@@ -187,7 +199,7 @@ void render_egl_image_dma_buf(void){
 }
 
 void swap_buffers(void){
-	unsigned int handle, pitch, fb;
+	unsigned int handle=0, pitch=0, fb=0;
 
 	eglSwapBuffers(eglDisplay, eglSurface);
 
@@ -216,13 +228,13 @@ void clean_kms(void){
 	drmModeFreeResources(kms.res);
 
 	if(gPrevBo){
-		drmModeRmFB(prevFb);
+		drmModeRmFB(fd, prevFb);
 		gbm_surface_release_buffer(gSurface, gPrevBo);
 	}
 }
 
 int main(int argc, char *argv[]){
-	fd = open("/dev/dri/renderD128", O_RDWR);
+	fd = open("/dev/dri/card0", O_RDWR);
 	if(fd < 0){
 		printf("Can't open fd\n");
 		return -1;
@@ -238,7 +250,7 @@ int main(int argc, char *argv[]){
 		return -1;
 	}
 
-	render_egl_image_dma_buf();
+	render_egl_image_dma_buf(kms.mode.hdisplay, kms.mode.vdisplay);
 
 	swap_buffers();
 
